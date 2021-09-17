63f7bb39a231ec9de26c7c17fdf9b01d62d03483
marcosihe <marcosherrera.ib@gmail.com>
2021-09-17 19:35:58 -0300

    Agregar maquetado del frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Amazone
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Carrito</a>
+            <a href="/signin">Ingresar</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">&copy; Todos los derechos reservados</footer>
+      </div>
+    </Router>
   );
 }
 
